c837113b593cd5fcd1ba42807e3c10a97fe514ce
wangshihui <shihui.wang@xblgroup.com>
2018-09-21 14:08:30 +0800

    123

--- src/views/edit-receipt/all-receipts.vue
+++ src/views/edit-receipt/all-receipts.vue
@@ -10,7 +10,7 @@
 					<span class="title">公寓租金收据</span>
 					<div class="receipt-number-date">
 						<p class="number">No {{item.id}}</p>
-						<p class="date">20{{year}}年{{month}}月{{date}}日</p>
+						<p class="date">20{{year}}年{{month}}月{{date}}日</p> 
 					</div>
 				</div>
 				<div class="receipt-content">
