3764325b23e9c51ba7d324f9c57035699ec4298f
wangshihui <shihui.wang@xblgroup.com>
2019-06-24 00:04:17 +0800

    更换脚手架为vuecli3,更换UI框架为饿了么，去掉不必要的部分

--- src/app.vue
+++ src/app.vue
@@ -1,40 +1,50 @@
 <template>
-    <div id="main" class="app-main">
-        <router-view></router-view>
-    </div>
+	<div id="app">
+		<c-menu></c-menu>
+		<router-view/>
+	</div>
 </template>
 
 <script>
-	import myIDB from './assets/js/indexedDB'
-
-    export default {
-        data () {
-            return {
-                theme: this.$store.state.app.themeColor
-            };
-        },
-        mounted () {
-
-        },
-        beforeDestroy () {
-
-        },
-        methods: {
-
-        }
-    };
+import cMenu from './components/cmenu'
+export default {
+	components: { cMenu }
+}
 </script>
 
-<style>
-html,body{
-    width: 100%;
-    height: 100%;
-    background: #f0f0f0;
-    overflow: hidden;
-    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+<style lang="scss">
+html, body {
+	margin: 0;
+	padding: 0;
+	width: 100%;
+	height: 100%;
+	background: #f0f0f0;
+	overflow: hidden;
 }
-.app-main{
-    width: 100%;
-    height: 100%;
+#app {
+	font-family: "微软雅黑";
+	height: 100%;
+	.el-table .el-table__body-wrapper tr, .el-table .el-table__fixed-body-wrapper tr {
+		height: 48px;
+	}
+	.page-content {
+		padding: 20px 10px;
+		.el-card {
+		}
+		.el-card__body {
+			overflow-y: scroll;
+			max-height: calc(100vh - 200px);
+			&::-webkit-scrollbar {
+				display:none
+			}
+		}
+	}
+	.search-container {
+		margin-bottom: 10px;
+		.el-input {
+			width: 200px;
+			margin-right: 10px
+		}
+	}
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,44 +1,36 @@
-import Vue from 'vue';
-import iView from 'iview';
-import {router} from './router/index';
-import {appRouter} from './router/router';
-import store from './store';
-import App from './app.vue';
-import '@/locale';
-import 'iview/dist/styles/iview.css';
-import VueI18n from 'vue-i18n';
-import util from './libs/util';
+import Vue from 'vue'
+import router from './router/index.js'
+import App from './App.vue'
+import { Button, Menu, Submenu, MenuItem, Dialog, Loading, Table,
+	TableColumn, Input, InputNumber, Card, Row, Col, Pagination,
+	Form, FormItem, Notification, Message, Upload, Image } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import { service } from './utils/request';
 
-Vue.use(VueI18n);
-Vue.use(iView);
+Vue.config.productionTip = false;
+Vue.prototype.$http = service;
+Vue.use(Button);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(Dialog);
+Vue.use(Loading);
+Vue.use(Table);
+Vue.use(Input);
+Vue.use(InputNumber);
+Vue.use(Card);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(TableColumn);
+Vue.use(Pagination);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Upload);
+Vue.use(Image);
+Vue.prototype.$notify = Notification;
+Vue.prototype.$message = Message;
 
 new Vue({
-    el: '#app',
-    router: router,
-    store: store,
+	router,
     render: h => h(App),
-    data: {
-        currentPageName: ''
-    },
-    mounted () {
-        this.currentPageName = this.$route.name;
-        // 显示打开的页面的列表
-        this.$store.commit('setOpenedList');
-        this.$store.commit('initCachepage');
-        // 权限菜单过滤相关
-        this.$store.commit('updateMenulist');
-        // iview-admin检查更新
-        util.checkUpdate(this);
-    },
-    created () {
-        let tagsList = [];
-        appRouter.map((item) => {
-            if (item.children.length <= 1) {
-                tagsList.push(item.children[0]);
-            } else {
-                tagsList.push(...item.children);
-            }
-        });
-        this.$store.commit('setTagsList', tagsList);
-    }
-});
+}).$mount('#app');
